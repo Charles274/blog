36ed6170fd7dfa91c5e15c8fc970c0e194a73c3a
Charles Owusu <charlesowusu274@gmail.com>
2024-11-17 15:29:52 +0000

    Install Flowbite and test it out

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 import type { Config } from "tailwindcss";
+import { plugin, content } from "flowbite-react/tailwind";
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    content(),
   ],
   theme: {
     extend: {
@@ -14,5 +17,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [plugin],
 } satisfies Config;
